0b90c891af99ca943f18680b4aa91a9b5d82b271
Albon Idrizi <65398478+albonidrizi@users.noreply.github.com>
2024-01-22 12:16:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+uimport React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+uuu
